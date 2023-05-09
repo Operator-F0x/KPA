3d26c1d857b7f169a741467da099c688b1fc3e8a
Daniele <daniele.devops@gmail.com>
2023-05-09 08:47:04 +0200

    Update load_database.py

--- Database_Strument/load_database.py
+++ Database_Strument/load_database.py
@@ -1,45 +1,38 @@
 import os
 from pykeepass import PyKeePass
 from Toolbox.initializer import initializer
-from Toolbox.regex_module import regex_password
-from Database_Strument.create_db import create_new_database
-from Toolbox.Hash_Gestion import get_password
-
-
-
-def check_password(Clear_Text):
-    # Check if the Clear_Text is valid
-    if not regex_password(Clear_Text):
-        print('Invalid Clear_Text. Please enter a non-empty Clear_Text that contains only alphanumeric characters and some special characters.')
-        return False
-    else:
-        return True
-
-
-
+from Database_Strument.create_db import create_KPA_database
+from Toolbox.Hash_Gestion import get_password, check_password
 
 def load_database():
-    # Get the current working directory, database directory, database file name, key directory, and key file name
+    # Get the database directory and database list
     db_dir, database_files, _, _ = initializer()
 
     # If no database exists, create a new one and prompt the user to create a key for it
     if not database_files:
         print('No database found. Please create a password for the new database.')
-        create_new_database()
+        print('Enter new Password for KPA Database --> ', end= '')
+        create_KPA_database()
+        _, database_files, _, _ = initializer() # I reinitialize the database list
+    # END IF
 
-    # Create a string containing the full path to the KeePass database file
+    print('KPA Database Loading'.center(30,'-'))
+    # Create a string containing the full path to the KPA.kdbx Database
     auto_database = os.path.join(db_dir, database_files[0])
-
+      
     # Prompt the user to enter the password until a valid one is entered
     while True:
+        print('Enter the Database Password --> ', end= '')
         password = get_password()
         if check_password(password):
             break
         else:
-            print('Wrong password')
+            print('Invalid Clear_Text. Please enter a non-empty Clear_Text that contains only alphanumeric characters and some special characters.')
 
-    print('Database Loaded')
-
+    print(' KPA - Database Loaded')
     kpa = PyKeePass(auto_database, password=password)
 
-    return kpa 
+    return kpa
+
+if __name__ == '__main__':
+    None
